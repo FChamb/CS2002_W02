d98d86525efbf062e92727752b7fc56fe22bc0b8
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2024-01-19 12:18:12 +0000

    Updated code base to support stage2

--- src/iterative_fib.c
+++ src/iterative_fib.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-#include "itfib.h"
+#include "fib.h"
 
 int fibcalc(int n) {
-
+    if (n <= 1) {
+        return n;
+    }
+    int fib = 1;
+    int prevFib = 1;
+    for (int i = 2; i < n; i++) {
+        int temp = fib;
+        fib += 2 * prevFib;
+        prevFib = temp;
+    }
+    return fib;
 }
