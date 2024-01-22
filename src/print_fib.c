45e0d8e2d1371c458eee1e126955827f1290b11b
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2024-01-22 19:14:59 +0000

    Updated print_fib.c to not accept non integer values and only positive values

--- src/print_fib.c
+++ src/print_fib.c
@@ -5,15 +5,19 @@
 void print_fib() {
     int length;
     printf("Length? ");
-    scanf("%d", &length);
-    printf("[");
-    int check = length - 1;
-    for (int i = 0; i < length; i++) {
-        if (i == check) {
-            printf("%d", fibcalc(i));
-        } else {
-            printf("%d, ", fibcalc(i));
+    if (scanf("%d", &length) != 1 || length < 0) {
+        printf("Invalid input\n");
+        return;
+    } else {
+        printf("[");
+        int check = length - 1;
+        for (int i = 0; i < length; i++) {
+            if (i == check) {
+                printf("%d", fibcalc(i));
+            } else {
+                printf("%d, ", fibcalc(i));
+            }
         }
+        printf("]");
     }
-    printf("]");
 }
